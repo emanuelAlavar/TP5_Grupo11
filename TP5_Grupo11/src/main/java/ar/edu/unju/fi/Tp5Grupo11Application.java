78ecbf0b953a89d3c036fe2d62c3185ddd1917b5
emanuelAlavar <manusovich15@gmail.com>
2022-05-06 20:49:04 -0300

    Repositorio TP5_Grupo11

---
+++ TP5_Grupo11/src/main/java/ar/edu/unju/fi/Tp5Grupo11Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5Grupo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp5Grupo11Application.class, args);
+	}
+
+}
